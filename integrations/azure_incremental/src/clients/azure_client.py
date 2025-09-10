e6907291a388df9e031caaa5a3203394bbcf0a3d
Melody Daniel <melodyogonna@gmail.com>
2025-09-10 13:47:51 +0100

    fix: Remove incorrect throttling handler from get_subscriptions

--- integrations/azure_incremental/src/clients/azure_client.py
+++ integrations/azure_incremental/src/clients/azure_client.py
@@ -41,19 +41,12 @@ class AzureClient:
             raise ValueError("Azure client not initialized")
 
         subscriptions: list[Subscription] = []
-        try:
-            async for sub in self.subs_client.subscriptions.list(
-                error_map=self._error_map
-            ):
-                await self._handle_rate_limit(self._rate_limiter.consume(1))
-                subscriptions.append(sub)
+        async for sub in self.subs_client.subscriptions.list():
+            await self._handle_rate_limit(self._rate_limiter.consume(1))
+            subscriptions.append(sub)
 
-            logger.info(f"Found {len(subscriptions)} subscriptions in Azure")
-            return subscriptions
-        except AzureRequestThrottled as e:
-            logger.warning("Azure request is getting throttled")
-            await e.handle_delay()
-            # AI! /ask there is a bug here, the throttled request don't seem to get retried
+        logger.info(f"Found {len(subscriptions)} subscriptions in Azure")
+        return subscriptions
 
     async def run_query(
         self, query: str, subscriptions: list[str]
@@ -87,7 +80,7 @@ class AzureClient:
                     break
                 logger.info("Fetching more data")
             except AzureRequestThrottled as e:
-                logger.warning("Azure request is getting throttled")
+                logger.warning("Azure request is getting throttled why running query")
                 await e.handle_delay()
 
     async def __aenter__(self) -> Self:
